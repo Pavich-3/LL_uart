511a8b8a900df69865e2f898499a217641a15154
Pavlo Baranchuk <pavlo.baranchuk@goreel.tech>
2025-09-29 22:56:50 +0300

    added init UART TX

--- Core/Inc/uart.h
+++ Core/Inc/uart.h
@@ -7,7 +7,7 @@ extern "C" {
 
 #include "main.h"
 
-void MX_USART1_UART_Init(void);
+void UART_Init(LL_USART_InitTypeDef* uartHandle);
 
 #ifdef __cplusplus
 }
--- Core/Src/uart.c
+++ Core/Src/uart.c
@@ -11,7 +11,16 @@ void UART_Init(LL_USART_InitTypeDef* uartHandle)
 			.OutputType = LL_GPIO_OUTPUT_PUSHPULL,
 			.Pull = LL_GPIO_PULL_NO,
 			.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH,
-			.Alternate = LL_GPIO_AF7
+			.Alternate = LL_GPIO_AF_7
 	};
 	LL_GPIO_Init(GPIOA, &GPIO_InitSturct);
+
+	uartHandle->BaudRate = 9600;
+	uartHandle->DataWidth = LL_USART_DATAWIDTH_8B;
+	uartHandle->StopBits = LL_USART_STOPBITS_1;
+	uartHandle->Parity = LL_USART_PARITY_NONE;
+	uartHandle->TransferDirection = LL_USART_DIRECTION_TX;
+	uartHandle->HardwareFlowControl = LL_USART_HWCONTROL_NONE;
+	uartHandle->OverSampling = LL_USART_OVERSAMPLING_16;
+	LL_USART_Init(USART1, uartHandle);
 }
